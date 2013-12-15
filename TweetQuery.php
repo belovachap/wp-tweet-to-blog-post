317c33c73cf066ddbadc2200b08f650470f46e5d
Chapman Shoop <chapman.shoop@gmail.com>
2013-12-15 14:44:20 -0800

    Dropping iTweetQuery interface
    The interface wasn't offering much over the abstraction class
    hierarchy so I'm dropping it at this point.

--- TweetQuery.php
+++ TweetQuery.php
@@ -1,70 +1,26 @@
 <?php
 
-interface iTweetQuery
-{
-	public function setQuery($query);
-	public function getQuery();
-
-	public function setMethod($method);
-	public function getMethod();
-
-	public function setStartFromTweetID($start_from_tweet_id);
-	public function getStartFromTweetID();
-
-	public function setMaxTweetsReturned($max_tweets_returned);
-	public function getMaxTweetsReturned();
-
-	public function getTweets($twitter_connetion);
-}
-
 abstract class TweetQueryMethod
 {
 	const SEARCH = 0;
 	const USER_TIMELINE = 1;
 
-/*
-	public static name_for_query_method($query_method) {
-		switch($query_method)
-	QUERY_METHOD_NAMES = array(
-		SEARCH => "Search",
+	public $QUERY_METHOD_NAMES = array(
+		SEARCH => "Twitter Search",
 		USER_TIMELINE => "User Timeline",
 	);
-*/
 }
 
-class SearchTweetQuery implements iTweetQuery 
-{
-	private $query;
-	private $method;
-	private $start_from_tweet_id;
-	private $max_tweets;
 
-	public function __construct($query, $method, $start_from_tweet_id, $max_tweets_returned)
+abstract class AbstractTweetQuery
+{
+	private $start_from_tweet_id;
+	private $max_tweets_returned;
+
+	public function __construct($start_from_tweet_id, $max_tweets_returned)
 	{
-		$this->setQuery($query);
-		$this->setMethod($method);
 		$this->setStartFromTweetID($start_from_tweet_id);
 		$this->setMaxTweetsReturned($max_tweets_returned);
-	}
-
-	public function setQuery($query)
-	{
-		$this->query = $query;
-	}
-
-	public function getQuery()
-	{
-		return $this->query;
-	}
-
-	public function setMethod($method)
-	{
-		$this->method = $method;
-	}
-
-	public function getMethod() 
-	{
-		return $this->method;
 	}
 
 	public function setStartFromTweetID($tweet_id)
@@ -87,11 +43,41 @@ class SearchTweetQuery implements iTweetQuery
 		return $this->max_tweets_returned;
 	}
 
+	abstract public function getMethod();
+	abstract public function getTweets($twitter_connection);
+}
+
+
+class SearchTweetQuery extends AbstractTweetQuery 
+{
+	private $search_query;
+
+	public function __construct($search_query, $start_from_tweet_id, $max_tweets_returned)
+	{
+		parent::__construct($start_from_tweet_id, $max_tweets_returned);
+		$this->setSearchQuery($search_query);
+	}
+
+	public function setSearchQuery($search_query)
+	{
+		$this->search_query = $search_query;
+	}
+
+	public function getSearchQuery()
+	{
+		return $this->search_query;
+	}
+
+	public function getMethod() 
+	{
+		return TweetQueryMethod::SEARCH;
+	}
+
 	public function getTweets($twitter_connection)
 	{
 		$search_parameters = array(
-			'q' => $this->getQuery(),
-			'since_id' => $this->getStartFromTweetID,
+			'q' => $this->getSearchQuery(),
+			'since_id' => $this->getStartFromTweetID(),
 			'include_entities' => true,
 			'count' => $this->getMaxTweetsReturned(),
 		);
@@ -99,6 +85,39 @@ class SearchTweetQuery implements iTweetQuery
 		$api_result = $twitter_connection->get('search/tweets', $search_parameters);
 
 		return $api_result->statuses;
+	}
+}
+
+
+class UserTimelineTweetQuery extends AbstractTweetQuery 
+{
+	private $screen_name;
+
+	public function __construct($screen_name, $start_from_tweet_id, $max_tweets_returned)
+	{
+		parent::__construct($start_from_tweet_id, $max_tweets_returned);
+		$this->setScreenName($screen_name);
+	}
+
+	public function setScreenName($screen_name)
+	{
+		$this->screen_name = $screen_name;
+	}
+
+	public function getScreenName()
+	{
+		return $this->screen_name;
+	}
+
+	public function getMethod() 
+	{
+		return TweetQueryMethod::USER_TIMELINE;
+	}
+
+	public function getTweets($twitter_connection)
+	{
+		// Retrieve tweets from screen_name's timeline
+		return;
 	}
 }
 
